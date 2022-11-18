ab0a9e0303943c5c01a07e2f1e760fe3493d447a
yhozan <65638054+yhozan@users.noreply.github.com>
2022-11-18 22:45:39 +0300

    Add files via upload

---
+++ ToolsforDataScienceProject.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# ### Yasin Hozan
+# Program Manager
+
+# *I am interested in Data Science because I love make meaning out of complicated situations. Dealing with the data and trying to analyze and understand it is kinda playing with your favorite toy as a child to me.*
+
+# ### This Following Code checks whether the input number is even or not
+
+# In[ ]:
+
+
+x= int(input("Please enter a number you want to test if it is even or not: "))
+
+if (x%2) == 0:
+    print("Your number is",x,"and even")
+else:
+    print("Your number is",x,"and not even")
+
+
+# *  #### One: This Course
+# *  #### Two: Is
+# *  #### Three: Great
+# 
+# 
+# | Country | Age | Job |
+# | --- | --- | --- |
+# | Turkey | 24 | Program Manager |
+# 
+# 
+# 
+# __[GitHub | AssignmentNotebook](https://github.com/yhozan/ToolsforDataScience)__
+# 
